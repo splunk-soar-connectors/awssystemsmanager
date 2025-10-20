486e70ff19bacec0f88fe30690e48c93c1e79b9f
mnordby-splunk <mnordby@splunk.com>
2025-10-20 14:55:44 -0600

    fix: update wheels for python 3.13 compatibility

--- awssystemsmanager_consts.py
+++ awssystemsmanager_consts.py
@@ -1,6 +1,6 @@
 # File: awssystemsmanager_consts.py
 #
-# Copyright (c) 2019-2024 Splunk Inc.
+# Copyright (c) 2019-2025 Splunk Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -22,31 +22,31 @@ SSM_JSON_DEFAULT_S3_BUCKET = "default_s3_bucket"
 SSM_JSON_BAD_ASSET_CONFIG_MSG = "Please provide access keys or select assume role check box in asset configuration"
 
 SSM_REGION_DICT = {
-        "US East (N. Virginia)": "us-east-1",
-        "US East (Ohio)": "us-east-2",
-        "US West (N. California)": "us-west-1",
-        "US West (Oregon)": "us-west-2",
-        "Asia Pacific (Hong Kong)": "ap-east-1",
-        "Asia Pacific (Mumbai)": "ap-south-1",
-        "Asia Pacific (Seoul)": "ap-northeast-2",
-        "Asia Pacific (Singapore)": "ap-southeast-1",
-        "Asia Pacific (Sydney)": "ap-southeast-2",
-        "Asia Pacific (Tokyo)": "ap-northeast-1",
-        "Canada (Central)": "ca-central-1",
-        "China (Beijing)": "cn-north-1",
-        "China (Ningxia)": "cn-northwest-1",
-        "EU (Frankfurt)": "eu-central-1",
-        "EU (Ireland)": "eu-west-1",
-        "EU (London)": "eu-west-2",
-        "EU (Paris)": "	eu-west-3",
-        "EU (Stockholm)": "eu-north-1",
-        "South America (Sao Paulo)": "sa-east-1",
-        "AWS GovCloud (US-East)": "us-gov-east-1",
-        "AWS GovCloud (US)": "us-gov-west-1"
-    }
+    "US East (N. Virginia)": "us-east-1",
+    "US East (Ohio)": "us-east-2",
+    "US West (N. California)": "us-west-1",
+    "US West (Oregon)": "us-west-2",
+    "Asia Pacific (Hong Kong)": "ap-east-1",
+    "Asia Pacific (Mumbai)": "ap-south-1",
+    "Asia Pacific (Seoul)": "ap-northeast-2",
+    "Asia Pacific (Singapore)": "ap-southeast-1",
+    "Asia Pacific (Sydney)": "ap-southeast-2",
+    "Asia Pacific (Tokyo)": "ap-northeast-1",
+    "Canada (Central)": "ca-central-1",
+    "China (Beijing)": "cn-north-1",
+    "China (Ningxia)": "cn-northwest-1",
+    "EU (Frankfurt)": "eu-central-1",
+    "EU (Ireland)": "eu-west-1",
+    "EU (London)": "eu-west-2",
+    "EU (Paris)": "	eu-west-3",
+    "EU (Stockholm)": "eu-north-1",
+    "South America (Sao Paulo)": "sa-east-1",
+    "AWS GovCloud (US-East)": "us-gov-east-1",
+    "AWS GovCloud (US)": "us-gov-west-1",
+}
 
-POWERSHELL_DOCUMENT = 'AWS-RunPowerShellScript'
-POWERSHELL_DOC_HASH = '2142e42a19e0955cc09e43600bf2e633df1917b69d2be9693737dfd62e0fdf61'  # pragma: allowlist secret
-LINUX_DOCUMENT = 'AWS-RunShellScript'
-LINUX_DOC_HASH = '99749de5e62f71e5ebe9a55c2321e2c394796afe7208cff048696541e6f6771e'  # pragma: allowlist secret
+POWERSHELL_DOCUMENT = "AWS-RunPowerShellScript"
+POWERSHELL_DOC_HASH = "2142e42a19e0955cc09e43600bf2e633df1917b69d2be9693737dfd62e0fdf61"  # pragma: allowlist secret
+LINUX_DOCUMENT = "AWS-RunShellScript"
+LINUX_DOC_HASH = "99749de5e62f71e5ebe9a55c2321e2c394796afe7208cff048696541e6f6771e"  # pragma: allowlist secret
 DEFAULT_REQUEST_TIMEOUT = 30  # in seconds
